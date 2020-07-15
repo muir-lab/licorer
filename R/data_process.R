9a62958b0aa80b178d5eb78bc035ac1f8c67bbd9
Daniel Trupp <daniel.r.trupp@gmail.com>
2020-07-15 16:33:26 -0700

    continuing work on the combine licor function

--- R/data_process.R
+++ R/data_process.R
@@ -102,24 +102,62 @@ get_remarks <- function(x) {
 
 combine_licor <- function(combo) {
   full <- combo[[1]]
+  temp <- attr(attributes(full)$names, "data_type")
+  full <- tibble::add_column(full, file_index = 1, .before = "obs")
+  attr(attributes(full)$names, "data_type") <- c("licor_file", temp)
+
   for (i in 2:length(combo)) {
+    temp <- attr(attributes(combo[[i]])$names, "data_type")
+    combo[[i]] <- tibble::add_column(combo[[i]], file_index = i, .before = "obs")
+    attr(attributes(combo[[i]])$names, "data_type") <- c("licor_file", temp)
+
     #make sure the data has the same amount of columns, fix if not
     if (any(subset_options(full) !=  subset_options(combo[[i]]))) {
-      #make the columns the same
-      #Should this be implemented? or should the user only combine two with the
-      #same variables?
+      #combo even with full
+      for (x in subset_options(full)) {
+        if (grep(x, subset_options(combo[[i]])) <= 0) {
+          for (y in attributes(full)$names[x]) {
+            temp <- attr(attributes(combo[[i]])$names, "data_type")
+            index <- grep(y, attributes(full)$names)
+            combo[[i]] <- tibble::add_column(combo[[i]], y = NA,
+                                             .after = index - 1)
+            attr(attributes(combo[[i]])$names, "data_type") <- c(temp[1:index],
+                                                                 x,
+                                                                 temp[index:lingth(temp)])
+          }
+        }
+      }
+
+      #full even with combo
+      for (x in subset_options(combo[[i]])) {
+        if (grep(x, subset_options(full)) <= 0) {
+          for (y in attributes(combo[[i]])$names[x]) {
+            temp <- attr(attributes(full)$names, "data_type")
+            index <- grep(y, attributes(full)$names)
+            full <- tibble::add_column(full, y = NA,
+                                       .after = index - 1)
+            attr(attributes(combo[[i]])$names, "data_type") <- c(temp[1:index],
+                                                                 x,
+                                                                 temp[index:lingth(temp)])
+          }
+        }
+      }
     }
 
+    temp <- attr(attributes(full)$names, "data_type")
     #combine the data
     full <- rbind(full, combo[[i]])
+    attr(attributes(full)$names, "data_type") <- temp
 
     #combine header datq
     for (j in 1:length(attributes(full)$header_data)) {
-      #should they only be added if they are different, or just add new values
       if (attributes(full)$header_data[[j]] != attributes(combo[[i]])$header_data[[j]]) {
         attributes(full)$header_data[[j]] <-
           append(attributes(full)$header_data[[j]], attributes(combo[[i]])$header_data[[j]])
       }
+    }
+    for (j in length(attributes(full)$header_data):length(attributes(combo[[i]])$header_data)) {
+
     }
   }
   invisible(full)
