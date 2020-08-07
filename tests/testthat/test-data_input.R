2024b628a48c3e878e8d4e52f9d06bc345e7e176
Daniel Trupp <daniel.r.trupp@gmail.com>
2020-08-07 11:57:18 -0700

    Updated unit tests for greater coverage of code.

--- tests/testthat/test-data_input.R
+++ tests/testthat/test-data_input.R
@@ -6,15 +6,30 @@ test_that("read_li6800_raw creates a licor object", {
                                    package = "licorer", mustWork = TRUE))
   expect_equal(class(x), c("licor", "tbl_df", "tbl", "data.frame"))
 
+  x <- read_li6800(system.file("extdata", "2019-05-06-0740_trillium_ovatum",
+                                   package = "licorer", mustWork = TRUE))
+  expect_equal(class(x), c("licor", "tbl_df", "tbl", "data.frame"))
   #Test reading in with a file path
   #x <- read_li6800_raw(rprojroot::find_root_file("2019-05-06-0740_trillium_ovatum"))
   #expect_equal(class(x), c("licor", "tbl_df", "tbl", "data.frame"))
 })
 
+test_that("read_li6400_raw creates a licor object", {
+  #Test reading in with an internal file
+  x <- read_li6400_raw(system.file("extdata", "chris0701_ACi_chi2",
+                                   package = "licorer", mustWork = TRUE))
+  expect_equal(class(x), c("licor", "tbl_df", "tbl", "data.frame"))
+
+  x <- read_li6400(system.file("extdata", "chris0701_ACi_chi2",
+                                   package = "licorer", mustWork = TRUE))
+  expect_equal(class(x), c("licor", "tbl_df", "tbl", "data.frame"))
+})
+
+
 test_that("licor object has units and a header attribute", {
   #This tests the validator, which determines that the object was created
   #with the correct components
-  x <- read_li6800_raw(system.file("extdata", "2019-05-06-0740_trillium_ovatum",
+  x <- licor(system.file("extdata", "2019-05-06-0740_trillium_ovatum",
                                    package = "licorer", mustWork = TRUE))
   expect_identical(validate_licor(x), x)
 })
